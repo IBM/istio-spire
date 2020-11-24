dafaa39fb81f23b1d44ceca5815d8198b08600eb
josselin <josselin.mi@pm.me>
2020-11-24 19:17:45 +0100

    fix: Fix the scripts for up to date environments
    * change the spire-agent regex in (6) to include all versions
    * change the name of the workload containers as they were not loaded
    * precise the shebang of the python2 script patch.py

--- 6_download_and_run_spire.sh
+++ 6_download_and_run_spire.sh
@@ -7,7 +7,7 @@ cd spire
 
 if [ ! -d spire-tutorials ]
   then git clone https://github.com/spiffe/spire-tutorials/
-  sed -i 's/gcr.io\/spiffe-io\/spire-agent:0.10.0/gcr.io\/spiffe-io\/spire-agent:1afb9bfd8fb4180d761e8c72de9e4740762ea1e7/g' spire-tutorials/k8s/quickstart/agent-daemonset.yaml
+  sed -i 's/gcr.io\/spiffe-io\/spire-agent:.*/gcr.io\/spiffe-io\/spire-agent:1afb9bfd8fb4180d761e8c72de9e4740762ea1e7/g' spire-tutorials/k8s/quickstart/agent-daemonset.yaml
 fi
 
 ./deploy_spire.sh
